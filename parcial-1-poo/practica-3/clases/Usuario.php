7196afb6ca0055c6c6d08fcaf5f4aa71785b0332
Marysa01 <161401484+Marysa01@users.noreply.github.com>
2026-03-09 02:50:38 -0700

    Add files via upload

---
+++ parcial-1-poo/practica-3/clases/Usuario.php
@@ -0,0 +1,42 @@
+<?php
+
+class Usuario {
+
+    private $nombre;
+    private $correo;
+
+    public function __construct($nombre, $correo) {
+
+        $this->nombre = $nombre;
+
+        // Validación del correo
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("El correo no tiene un formato válido");
+        }
+
+        $this->correo = $correo;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function getCorreo(){
+        return $this->correo;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function setCorreo($correo){
+
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("El correo no tiene un formato válido");
+        }
+
+        $this->correo = $correo;
+    }
+}
+
+?>
